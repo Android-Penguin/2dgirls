f5c26c01a2dd1dc107514c0693c31d043c36f663
The_Android_Penguin <49669409+AndroidPenguin@users.noreply.github.com>
2020-09-04 22:26:04 +1200

    Autowriting gallery
    - Created JSON array with image metadata.
    
    - Added code to automatically create HTML for image gallery.
    
    - Uploaded a few more images.

---
+++ image_list.js
@@ -0,0 +1,33 @@
+var image_list = [
+  {"item_number":1, "name":"Nino", "show":"5 toubun",
+    "filepath":"content_images/nino_small.png",
+    "onclick":"modalOpen('content_images/nino.png')"},
+
+  {"item_number":2, "name":"Rem", "show":"Re:Zero",
+    "filepath":"content_images/rem_small.png",
+    "onclick":"modalOpen('content_images/rem.png')"},
+
+  {"item_number":3, "name":"Felix", "show":"Re:Zero",
+    "filepath":"content_images/felix_small.png",
+    "onclick":"modalOpen('content_images/felix.png')"},
+
+  {"item_number":4, "name":"Astolfo", "show":"Fate/stay night",
+    "filepath":"content_images/astolfo_small.png",
+    "onclick":"modalOpen('content_images/astolfo.png')"},
+
+  {"item_number":5, "name":"Mai Sakurajima", "show":"Bunny Girl Senpai",
+    "filepath":"content_images/mai-sakurajima_small.png",
+    "onclick":"modalOpen('content_images/mai-sakurajima.png')"},
+
+  {"item_number":6, "name":"Asuna", "show":"Sword Art Online",
+    "filepath":"content_images/asuna_small.png",
+    "onclick":"modalOpen('content_images/asuna.png')"},
+  //
+  // {"item_number":, "name":"_",
+  //   "filepath":"content_images/_small.png",
+  //   "onclick":"modalOpen('content_images/.png')"},
+  //
+  // {"item_number":, "name":"_",
+  //   "filepath":"content_images/_small.png",
+  //   "onclick":"modalOpen('content_images/.png')"},
+];
