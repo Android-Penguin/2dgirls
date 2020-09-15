236cc10bd299a2f6c8c599c12a1d36ed2460c1d9
The_Android_Penguin <49669409+AndroidPenguin@users.noreply.github.com>
2020-09-15 20:42:44 +1200

    Online JS test 3
    - Changed images in JSON arrays.

---
+++ image_list2.js
@@ -0,0 +1,25 @@
+var image_list = [
+  {"item_number":1, "name":"Penugin", "show":"none",
+    "filepath":"content_images/test_penguin.png",
+    "onclick":"modalOpen('content_images/test_penguin.png')"},
+
+  {"item_number":1, "name":"Penugin", "show":"none",
+      "filepath":"content_images/test_penguin.png",
+      "onclick":"modalOpen('content_images/test_penguin.png')"},
+
+  {"item_number":1, "name":"Penugin", "show":"none",
+    "filepath":"content_images/test_penguin.png",
+    "onclick":"modalOpen('content_images/test_penguin.png')"},
+
+  {"item_number":1, "name":"Penugin", "show":"none",
+    "filepath":"content_images/test_penguin.png",
+    "onclick":"modalOpen('content_images/test_penguin.png')"},
+
+  {"item_number":1, "name":"Penugin", "show":"none",
+      "filepath":"content_images/test_penguin.png",
+      "onclick":"modalOpen('content_images/test_penguin.png')"},
+
+  {"item_number":1, "name":"Penugin", "show":"none",
+    "filepath":"content_images/test_penguin.png",
+    "onclick":"modalOpen('content_images/test_penguin.png')"},
+];
